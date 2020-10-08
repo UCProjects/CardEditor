224fb49788072822454de0fb47872ace0b6f6867
feildmaster <feildmaster@users.noreply.github.com>
2020-10-07 21:01:39 -0500

    chore: new keywords

--- public/resources/modules/effects.js
+++ public/resources/modules/effects.js
@@ -1,10 +1,12 @@
 export const effects = [
   // Keywords
   'Armor', 'Candy', 'Charge', 'Disarmed', 'Dodge', 'Dust', 'Fatigue', 'Future', 'Haste', 'Invulnerable', 'Magic', 'Paralyze', 'Ranged', 'Silence', 'Support', 'Taunt', 'Transparency', 'Turbo', 'Turn end', 'Turn start', 'Thorns',
-  'Synergy',
+  'Synergy', 'Shock', 'Delay',
   // Tribes
   'Amalgamates?', 'Bombs?', 'Dogs?', 'Froggits?', 'G Followers?', 'Lost Souls?', 'Molds?', 'Plants?', 'Royal Guards?', 'Snails?', 'Spiders?', 'Temmies?', 'Chaos Weapons?',
   'Arachnids?', 'Pieces?', 'Royal Inventions?',
+  // Other
+  `Gerson's Artifact`, 'Genocide', 'Outbreak',
 ];
 export const specials = ['ATK', 'DMG', 'HP', 'KR', 'cost', 'G'];
 
