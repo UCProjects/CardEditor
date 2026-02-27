50937567a9134c8df88bf4b892731c9bd5b00cc0
feildmaster <feildmaster@users.noreply.github.com>
2026-02-26 20:45:02 -0600

    fix: add base element

---
+++ public/resources/modules/elements/base.js
@@ -0,0 +1,45 @@
+import { v4 } from 'https://jspm.dev/npm:uuid@9.0.0';
+
+export default class Base {
+  #id;
+  #type;
+
+  constructor({
+    id = v4(),
+    type,
+  }) {
+    if (!type) throw new Error('Element requires type');
+    this.#id = id;
+    this.#type = type;
+  }
+
+
+  get id() {
+    return this.#id;
+  }
+
+  get type() {
+    return this.#type;
+  }
+
+  getElement(id = `template#${this.type}`) {
+    const template = document.querySelector(id);
+    if (!template) throw new Error(`Failed to find template '${id}'`);
+    const container = document.createElement('div');
+    container.id = this.id;
+    container.innerHTML = template.innerHTML;
+    container.classList.add('element', this.type);
+    return container;
+  }
+
+  toJSON() {
+    const {
+      id,
+      type,
+    } = this;
+    return {
+      id,
+      type,
+    };
+  }
+}
