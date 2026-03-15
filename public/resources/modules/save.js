a0271783f712ffc6a38f6dfebceec59b904184f3
feildmaster <feildmaster@users.noreply.github.com>
2026-03-15 13:04:38 -0500

    chore: migrate to html-to-image

--- public/resources/modules/save.js
+++ public/resources/modules/save.js
@@ -1,24 +1,20 @@
-/* global domtoimage */
-export default function save(element, as) {
-  element.classList.toggle('saving', true);
-  image(element).then((url) => {
-    const link = document.createElement('a');
-    link.download = `${as || element.querySelector('.name input').value || 'undercard'}.png`;
-    link.href = url;
-    link.click();
-  }).catch((error) => console.error('Ooops.', error))
-    .then(() => {
-      element.classList.toggle('saving', false);
-    });
-}
+import { toPng } from 'https://ga.jspm.io/npm:html-to-image@1.11.13/es/index.js';
+import style from '../styles/saving.css' with { type: 'css' };
+import { tryOrErrorAsync } from './toast/index.js';
 
-export function image(element) {
-  return domtoimage.toPng(element, { filter });
+document.adoptedStyleSheets.push(style);
+
+export default async function save(element, as) {
+  element.classList.add('saving');
+  const url = await tryOrErrorAsync(() => toPng(element, { filter, backgroundColor: 'var(--background)' }), 'Failed to convert to image');
+  element.classList.remove('saving');
+  if (!url) return;
+  const link = document.createElement('a');
+  link.download = `${as || 'undercard'}.png`;
+  link.href = url;
+  link.click();
 }
 
 function filter(node) {
-  if (node.classList && node.classList.contains('no-save')) {
-    return false;
-  }
-  return (node.nodeName !== 'IMG' || node.src);
+  return !node.classList?.contains('no-save') && (node.nodeName !== 'IMG' || new URL(node.src).pathname !== '/');
 }
