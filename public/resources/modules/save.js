e9f2598f53657f3e91be9a6d20e3cbd28b04b711
feildmaster <feildmaster@users.noreply.github.com>
2026-03-16 21:56:12 -0500

    fix: tryOrError is async now

--- public/resources/modules/save.js
+++ public/resources/modules/save.js
@@ -1,12 +1,15 @@
 import { toPng } from 'https://ga.jspm.io/npm:html-to-image@1.11.13/es/index.js';
 import style from '../styles/saving.css' with { type: 'css' };
-import { tryOrErrorAsync } from './toast/index.js';
+import { tryOrError } from './toast/index.js';
 
 document.adoptedStyleSheets.push(style);
 
 export default async function save(element, as) {
   element.classList.add('saving');
-  const url = await tryOrErrorAsync(() => toPng(element, { filter, backgroundColor: 'var(--background)' }), 'Failed to convert to image');
+  const url = await tryOrError(
+    () => toPng(element, { filter, backgroundColor: 'var(--background)' }),
+    'Failed to convert to image',
+  );
   element.classList.remove('saving');
   if (!url) return;
   const link = document.createElement('a');
