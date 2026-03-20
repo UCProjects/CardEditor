4bb94bde82eeb04e8cb9503ded41e4b33f1ba40e
feildmaster <feildmaster@users.noreply.github.com>
2026-03-20 17:02:47 -0500

    feat: text can be resized

--- public/resources/modules/elements/TextElement.js
+++ public/resources/modules/elements/TextElement.js
@@ -1,8 +1,18 @@
 import BaseElement from './ImageElement.js';
 import { Elements } from './types.js';
 
+export const TextSizes = Object.freeze({
+  Normal: '',
+  Stretch: 'stretch',
+  Short: 'short',
+});
+
 export default class TextElement extends BaseElement {
+  /** @type {TextSizes[keyof TextSizes]} */
+  #size;
+
   constructor({
+      size,
       ...rest
     } = {}) {
     super({
@@ -10,5 +20,23 @@ export default class TextElement extends BaseElement {
       ...rest,
       type: Elements.Text,
     });
+    this.size = size;
+  }
+
+  get size() {
+    return this.#size || undefined;
+  }
+
+  set size(size = '') {
+    if (!Object.values(TextSizes).includes(size)) return;
+    this.#size = size;
+  }
+
+  toJSON() {
+    const { size } = this;
+    return {
+      ...super.toJSON(),
+      size,
+    };
   }
 }
