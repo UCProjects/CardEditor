2d0e55f98c6bc9c54203e255d026d754ec98a9ec
feildmaster <feildmaster@users.noreply.github.com>
2026-03-20 17:59:41 -0500

    fix: downloading

--- scripts/downloadImages.js
+++ scripts/downloadImages.js
@@ -21,9 +21,6 @@ const effects = new Set([
 ]);
 
 async function updateFile(path, data) {
-  // Create blank if not exists
-  await fs.writeFile(path, Array.isArray(data) ? [] : {}, { flag: 'wx' });
-
   // Load existing
   const file = await fs.readFile(path);
   const existing = JSON.parse(file.toString());
@@ -45,14 +42,15 @@ async function download(url, file) {
   try {
     const image = await fetch(url);
     await fs.writeFile(file, image.body);
-  } catch {
-    console.error('Failed to save', basename(file));
+  } catch (e) {
+    console.error('Failed to save', basename(file), e.message || e);
   }
 }
 
 async function downloadAvatars(images) {
   const path = join(base, 'images', 'avatars');
-  for (const name in images) {
+  await fs.mkdir(path, { recursive: true });
+  for (const name of images) {
     const url = `https://undercards.net/images/cards/${name}.png`;
     await download(url, resolve(path, `${name}.png`));
   }
@@ -60,8 +58,9 @@ async function downloadAvatars(images) {
 
 async function downloadEffects() {
   const path = join(base, 'images', 'effects');
+  await fs.mkdir(path, { recursive: true });
   const values = effects.values();
-  for (const effect in values) {
+  for (const effect of values) {
     const url = `https://undercards.net/images/powers/${effect}.png`;
     await download(url, resolve(path, `${effect}.png`));
   }
@@ -76,7 +75,7 @@ fetch('https://undercards.net/AllCards')
   })))
   .then((avatars) => Promise.all([
     updateFile(resolve(base, 'data', 'avatars.json'), avatars),
-    updateFile(resolve(base, 'data', 'effects.json'), effects.values()),
+    updateFile(resolve(base, 'data', 'status.json'), effects.values()),
     downloadAvatars(Object.values(avatars)),
     downloadEffects(),
   ]));
