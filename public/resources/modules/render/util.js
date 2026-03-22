c99fe54da96b405a046807c34115edd68a5341ce
feildmaster <feildmaster@users.noreply.github.com>
2026-03-21 20:53:19 -0500

    feat: stats and switch commands

--- public/resources/modules/render/util.js
+++ public/resources/modules/render/util.js
@@ -2,9 +2,23 @@ import { keywords, specials } from '../keywords.js';
 import Builder from '../utils/builder.js';
 
 const underlineRegex = new Builder(() => new RegExp(`(?<!\\\\)(${keywords.join('|')})(?![^{]*})|_([^_]+)_`, 'g'));
-const specialRegex = new RegExp(`(?<!{|>|\\w|\\\\)(${specials.join('|')})(?![\\w}])`, 'g');
-const colorRegex = /\{([^;|}]*)[^}]*[;|]([^}]+)}/g; // /\{color:([^}]+)}(.*){\/color}/g;
-const highlightRegex = /\{([^}]+)}/g;
+const specialRegex = new RegExp(`(?<!{|"|>|\\w|\\\\)(${specials.join('|')})(?![\\w}])`, 'g');
+const colorRegex = /(?<!{|\\){(?!{)([^;|}]*)[^}]*[;|]([^}]+)}/g; // /\{color:([^}]+)}(.*){\/color}/g;
+const highlightRegex = /(?<!{|\\){(?!{)([^}]+)}/g;
+const commandRegex = /{{([^}]*)}}/g;
+
+const commands = {
+  // image() {},
+  stats(args) {
+    return ['COST', 'attack', 'health']
+      .slice(Math.max(0, 3 - args.length))
+      .map((clazz, i) => args[i].replace(/\d+/, `<span class="${clazz}">$&</span>`))
+      .join('/');
+  },
+  switch([text, direction = 'left']) {
+    return `<span class="switch${direction.startsWith('r') ? 'Right' : 'Left'}">${text}</span>`;
+  }
+};
 
 export function getHTMLDescription(description = '') {
   return description
@@ -12,11 +26,24 @@ export function getHTMLDescription(description = '') {
     .replaceAll('"', '&quot;')
     .replaceAll('<', '&lt;')
     .replaceAll('>', '&gt;')
+    .replace(commandRegex, (_, $1) => process($1))
     .replace(underlineRegex.value, (_, $1, $2) => `<span class="underline">${$2 || $1}</span>`)
     .replace(colorRegex, (_, $1, $2) => `<span style="color:${$1}">${$2}</span>`)
     .replace(specialRegex, (_, $1) => `<span class="${getClass($1)}">${$1}</span>`)
     .replace(highlightRegex, (_, $1) => `<span class="cardName">${$1}</span>`)
     .replaceAll('\\', '');
+}
+
+function process(text = '') {
+  const [first = '', ...args] = text.split('|').filter(_ => _);
+  const [command, ...rest] = first.split(':');
+  if (rest.length) {
+    const add = rest.join(':');
+    if (add) args.unshift(add);
+  }
+  const handler = commands[command.toLowerCase()];
+  if (typeof handler !== 'function' || !args.length) return '';
+  return handler(args);
 }
 
 function getClass(keyword) {
