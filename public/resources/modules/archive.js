0d4cb04c42689efd1b7d88413924fbd999de02a8
feildmaster <feildmaster@users.noreply.github.com>
2026-03-23 22:02:54 -0500

    wip: archive

---
+++ public/resources/modules/archive.js
@@ -0,0 +1,30 @@
+import style from '../styles/archive.css' with { type: 'css' };
+
+document.adoptedStyleSheets.push(style);
+
+/** @type {HTMLDivElement} */
+const button = document.querySelector('.archive-button');
+/** @type {HTMLDivElement} */
+const archive = document.querySelector('.archive');
+
+button.addEventListener('click', () => {
+  archive.showPopover();
+});
+
+function setActive(page) {
+  const el = archive.querySelector(`div[data-page="${page}"]`);
+  if (el.matches('.active')) return;
+  archive.querySelector('.active').classList.remove('active');
+  el.classList.add('active');
+}
+
+archive.querySelectorAll('input[name="page"]').forEach((el) => {
+  el.addEventListener('change', () => setActive(el.id));
+});
+
+// Get all items
+// hide items being used
+// add new items, hidden
+// Add delete button
+// Drag onto group?
+// Drag into archive?
