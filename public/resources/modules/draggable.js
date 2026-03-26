703b99dea90a2ac8c539e78a97d36dc994e91262
feildmaster <feildmaster@users.noreply.github.com>
2026-03-25 20:17:30 -0500

    feat: sorting, sorted

--- public/resources/modules/draggable.js
+++ public/resources/modules/draggable.js
@@ -1,24 +1,59 @@
 import { Draggable, Sortable } from 'https://ga.jspm.io/npm:@shopify/draggable@1.2.1/build/esm/index.mjs';
+import { get, register, save } from './elements/registry.js';
+import { swap } from './utils/array.js';
 
-const instance = new Sortable([], {
+/** @typedef {import('./elements/GroupElement.js').default} GroupElement */
+
+const exclude = {
+  plugins: [Draggable.Plugins.Focusable, Draggable.Plugins.Announcement],
+};
+
+export const sortGroup = new Sortable([document.getElementById('app')], {
+  draggable: '.element.group',
+  handle: '.element.group .handle',
+  classes: {
+    'body:dragging': 'group-dragging',
+    'source:dragging': 'dragging',
+  },
+  mirror: {
+    constrainDimensions: true,
+  },
+  exclude,
+});
+
+const sortElement = new Sortable([], {
   draggable: '.element',
+  handle: '.handlee',
   classes: {
     'source:dragging': 'dragging',
   },
-  // handle: '.name',
-}).removePlugin(Draggable.Plugins.Focusable, Draggable.Plugins.Announcement)
-  // .removeSensor(Sensors.TouchSensor)
-  .on('drag:start', (e) => {
-    // if (!e.sourceContainer.parentElement.classList.contains('sortmode')) e.cancel();
-  });
+  exclude,
+}).on('sortable:stop', (e) => {
+  /** @type {GroupElement} */
+  const from = get(e.oldContainer.closest('[data-id]').dataset.id);
 
-export default function setup(group) {
-  instance.addContainer(group.querySelector('.cards'));
-  group.querySelector('.sidebar .sort').parentElement.onclick = () => {
-    group.classList.toggle('sortmode');
-  };
+  if (e.oldContainer === e.newContainer) {
+    swap(from.content, e.oldIndex, e.newIndex);
+  } else {
+    /** @type {GroupElement} */
+    const to = get(e.newContainer.closest('[data-id]').dataset.id);
+    const [moved] = from.content.splice(e.oldIndex, 1);
+    to.content.splice(e.newIndex, 0, moved);
+    save(to.id);
+  }
+
+  save(from.id);
+});
+
+/** @param {import('./render/GroupRenderer.js').default} group */
+export default function setup({ container, element }) {
+  sortElement.addContainer(container.querySelector('.content'));
+  sortElement.on('sortable:sorted', () => {
+    if (get(element.id)) return;
+    register(element);
+  });
 }
 
 export function isDragging() {
-  return instance.isDragging();
+  return sortElement.isDragging() || sortGroup.isDragging();
 }
