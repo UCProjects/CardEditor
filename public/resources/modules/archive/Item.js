cc68ca4067ac5ba2afc50d4994d0f5d822ddff87
feildmaster <feildmaster@users.noreply.github.com>
2026-04-05 21:21:33 -0500

    fix: trash/restore/drop interactions

--- public/resources/modules/archive/Item.js
+++ public/resources/modules/archive/Item.js
@@ -15,7 +15,10 @@ export default class Item extends EventEmitter {
 
     this.element
       .on('archived', () => this.emit('archived'))
-      .on('delete', () => this.emit('trash'))
+      .on('delete', () => {
+        this.emit('archived');
+        this.emit('trash');
+      })
       .on('newGroup', (newGroup) => this.group = newGroup)
       .on('updated', () => this.emit('update'));
   }
