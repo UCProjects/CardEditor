8366a7ba64d485345740f77890d8c57ea3ffe6a1
feildmaster <feildmaster@users.noreply.github.com>
2026-09-13 18:14:09 -0500

    fix: specify config as mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -109,7 +109,7 @@ export default defineConfig([
     },
   },
   {
-    files: ['./eslint.config.js'],
+    files: ['./eslint.config.mjs'],
     languageOptions: {
       ecmaVersion: 'latest',
       globals: {
